e81477811c1efba68162624de3d864657070f014
Esteban Ariza Acosta <48232743+Esarac@users.noreply.github.com>
2022-05-07 19:58:20 -0500

    footer :footprints:

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,21 @@
 import Title from './slides/Title';
 // import Functionalities from './slides/Functionalities'
 import Examples from './slides/Examples'
-
 import Page from '../components/page/Page'
 
+import ReactGa from 'react-ga'
+import { useEffect } from 'react';
+
+import {FaInstagram} from 'react-icons/fa'
+
 function App() {
+  useEffect(()=>{
+    ReactGa.initialize('G-4X9Z8VGM86');
+
+    //To report page view
+    ReactGa.pageview('/')
+  }, [])
+
   return (
     <>
         <Page theme='light' page={0}>
@@ -18,6 +29,22 @@ function App() {
         <Page theme='light' page={2}>
             <Examples/>
         </Page>
+
+        <div className='footer text-light'>
+          <p className='text-center h1 display-1'>
+            <a href='https://www.instagram.com/esaracgp/'>
+              <FaInstagram className='text-light'/>
+            </a>
+          </p>
+          <div className='d-flex justify-content-between'>
+            <p><small>
+              © 2022 Personal Kitchen Club. All Rights Reserved
+            </small></p>
+            <p><small>
+              This website uses cookies in order to offer you the most relevant information.
+            </small></p>
+          </div>
+        </div>
     </>
   );
 }
